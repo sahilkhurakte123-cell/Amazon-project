addc19a1aadbdda13479f7455e203e29dcf83dbd
skind <sahilkhurakte123@gmail.com>
2025-12-21 18:06:50 +0530

    Save and generate deliver options html using js

--- data/cart.js
+++ data/cart.js
@@ -4,11 +4,13 @@ if (!cart) {
   cart = [
     {
       productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-      quantity: 2
+      quantity: 2,
+      deliveryOptionId:'1'
     },
     {
       productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-      quantity: 2
+      quantity: 2,
+      deliveryOptionId:'2'
     }
   ];
   
@@ -19,7 +21,7 @@ function saveToStorage(){
   localStorage.setItem('cart', JSON.stringify(cart));
 }
 
-export function addToCart(productId,productName)
+export function addToCart(productId,productName,deliveryOptionId)
 {
   let matchingItem;
 
@@ -36,7 +38,8 @@ export function addToCart(productId,productName)
     cart.push({
       productId: productId,
       productName: productName,
-      quantity: 1
+      quantity: 1,
+      deliveryOptionId:'1'
     });
   }
   saveToStorage();
